7a037ad93f9142b36633dcba8077ff423869aea8
Manqoba Trevon Mkhize <manqobamkhize859@gmail.com>
2025-02-07 09:26:45 +0200

    Learning C-Sharp

---
+++ C-Sharp/C-Sharp-Course/Bro-Code/Myfirstprogram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C-Sharp/C-Sharp-Course/Bro-Code/Output.cs
@@ -0,0 +1,34 @@
+class MyFirstProgram
+{
+    static void Main(string[] args)
+    {
+        //This is a Comment
+        /*
+        This 
+        is 
+        a
+        multilint
+        comment
+        */           
+        Console.Write("Hi")  ;
+        
+        Console.WriteLine("Hello !");
+        /*
+        Escape Sequence  |     Representation
+        -----------------|-------------------
+        \a  Bell
+        \b Backspace
+        \f form feed
+        \n new line
+        \r carriage return
+        \t horizontal tab
+        \v vertical tab
+        \' single quotation mark
+        '" double quotation mark
+        \\ Backslash
+        \? literal question mark
+        */
+        Console.WriteLine("Trevon")
+        Console.ReadKey();
+    }
+}
